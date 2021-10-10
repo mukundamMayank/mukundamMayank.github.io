64779647bbe09e140cd3fc9e9034cd1afd338eaa
mukundamMayank <mayank.mukundam@students.iiit.ac.in>
2021-10-10 23:12:49 +0530

    Portfolio Website

---
+++ index.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    startTime();
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+    for (let i = 0; i < tab_switchers.length; i++) {
+        const tab_switcher = tab_switchers[i];
+        const page_id = tab_switcher.dataset.tab;
+
+        tab_switcher.addEventListener('click', () => {
+            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+            tab_switcher.parentNode.classList.add('is-active');
+
+            switchPage(page_id);
+        });
+    }
+}
+
+function switchPage (page_id) {
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("time").innerHTML=  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+  function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
